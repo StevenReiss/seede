f22f9269af27ae3a7954f11740855f8dc2ec0575
Steven Reiss <spr@cs.brown.edu>
2016-10-03 13:49:32 -0400

    Add initial build files and empty code file for sesame.

---
+++ javasrc/edu/brown/cs/seede/sesame/SesameConstants.java
@@ -0,0 +1,38 @@
+/********************************************************************************/
+/*										*/
+/*		SesameConstants.java						*/
+/*										*/
+/*	SEEDE Management Environment constant definitions			*/
+/*										*/
+/********************************************************************************/
+/*	Copyright 2016 Brown University -- Steven P. Reiss		      */
+/*********************************************************************************
+ *  Copyright 2011, Brown University, Providence, RI.				 *
+ *										 *
+ *			  All Rights Reserved					 *
+ *										 *
+ * This program and the accompanying materials are made available under the	 *
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution, *
+ * and is available at								 *
+ *	http://www.eclipse.org/legal/epl-v10.html				 *
+ *										 *
+ ********************************************************************************/
+
+
+
+package edu.brown.cs.seede.sesame;
+
+
+
+public interface SesameConstants {
+
+
+
+
+
+}	// end of interface SesameConstants
+
+
+
+
+/* end of SesameConstants.java */
